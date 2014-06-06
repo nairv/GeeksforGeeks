7facaa9b02b73bedb7ba68e05f2057c7d6daebcc
vineet <vineet.rnair@GMAIL.COM>
2014-06-06 16:55:28 -0700

    bin added

--- src/trees/LevelOrderTraversal.java
+++ src/trees/LevelOrderTraversal.java
@@ -1,7 +1,19 @@
 package trees;
 
 public class LevelOrderTraversal {
-	public static void main(String args[]){
+	
+	public static void printLevelOrder(Node root){
 		
+	}
+	public static void main(String args[]){
+		Node root = new Node(10);
+		Node l1 = new Node(5);
+		Node r1 = new Node(15);
+		root.setLeft(l1);
+		root.setRight(r1);
+		l1.setLeft(new Node(2));
+		l1.setRight(new Node(8));
+		
+		printLevelOrder(root);
 	}
 }
