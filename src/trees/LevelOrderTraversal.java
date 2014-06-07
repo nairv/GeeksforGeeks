4ae23b6672d87823559b0c0c45d948d59bd07779
vineet <vineet.rnair@GMAIL.COM>
2014-06-06 17:52:45 -0700

    After Level Order

--- src/trees/LevelOrderTraversal.java
+++ src/trees/LevelOrderTraversal.java
@@ -1,9 +1,19 @@
 package trees;
-
+import java.util.*;
 public class LevelOrderTraversal {
 	
 	public static void printLevelOrder(Node root){
+		ArrayDeque<Node> q1 = new ArrayDeque<Node>();
+		q1.add(root);
 		
+		while(!q1.isEmpty()){
+			Node t1 = q1.removeFirst();
+			System.out.print(t1.getData() + "\t");
+			if(t1.getLeft() != null)
+				q1.add(t1.getLeft());
+			if(t1.getRight() != null)
+				q1.add(t1.getRight());
+		}
 	}
 	public static void main(String args[]){
 		Node root = new Node(10);
