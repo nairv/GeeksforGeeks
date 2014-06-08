1c68a0cfdeb5dd5df45e0a24b035222e309f30c7
Vineet Nair <vineet.rnair@gmail.com>
2014-06-08 09:03:59 -0700

    TestClass added

---
+++ test/trees/LowestCommonAncestorTest.java
@@ -0,0 +1,25 @@
+package trees;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by vineet on 6/8/14.
+ */
+public class LowestCommonAncestorTest {
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void testLca() throws Exception {
+
+  }
+}
