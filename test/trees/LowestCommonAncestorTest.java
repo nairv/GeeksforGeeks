ed9ad6fd365dd19326d3ebfd1f85e71ac3eab749
Vineet Nair <vineet.rnair@gmail.com>
2014-06-08 09:16:27 -0700

    Test Infra added

--- test/trees/LowestCommonAncestorTest.java
+++ test/trees/LowestCommonAncestorTest.java
@@ -1,9 +1,11 @@
 package trees;
 
+import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import trees.LowestCommonAncestor;
+import static org.junit.Assert.assertEquals;
 /**
  * Created by vineet on 6/8/14.
  */
@@ -20,6 +22,9 @@ public class LowestCommonAncestorTest {
 
   @Test
   public void testLca() throws Exception {
-
+    BinaryTree bt = new BinaryTree();
+    assertEquals("The test passed" , LowestCommonAncestor.lca(bt.root , 5 , 18) , 10);
+    //Assert.failNotEquals();
+    Assert.assertNotSame("The test should fail" , LowestCommonAncestor.lca(bt.root , 15 , 12) , 10);
   }
 }
