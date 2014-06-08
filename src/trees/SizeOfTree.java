90013940c4f480fcde36eba2eaca5a92ab866b75
Vineet Nair <vineet.rnair@gmail.com>
2014-06-07 18:53:56 -0700

    After SizeOf Tree

--- src/trees/SizeOfTree.java
+++ src/trees/SizeOfTree.java
@@ -4,4 +4,16 @@ package trees;
  * Created by vineet on 6/7/14.
  */
 public class SizeOfTree {
+  public static int printSize(Node root){
+    if(root == null)
+      return 0;
+    else{
+      return 1 + printSize(root.getRight()) + printSize(root.getLeft());
+    }
+  }
+
+  public static void main(String args[]){
+    BinaryTree bt = new BinaryTree();
+    System.out.println("Size of the Tree :" + printSize(bt.root));
+  }
 }
