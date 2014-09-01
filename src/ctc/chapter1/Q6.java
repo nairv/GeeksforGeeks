3465c5ed63546987b8e504806fb06aa5503706d7
Vineet Nair <vineet.rnair@gmail.com>
2014-09-01 15:05:49 -0700

    CTC matrix rotation

--- src/ctc/chapter1/Q6.java
+++ src/ctc/chapter1/Q6.java
@@ -10,16 +10,16 @@ import java.util.Random;
  */
 public class Q6 {
 
-  public doRotation(int a[][] , int num){
+  public static void doRotation(int a[][] , int num){
     for (int i = 0; i < num; i++){
-      doRotationOnce(a , a.length);
+      doRotationOnce(a );
     }
   }
   /*
   a[][] - The N x N matrix
   n - length of each side of the matrix
    */
-  public void doRotationOnce(int a[][]  , int n){
+  public static void doRotationOnce(int a[][]){
     int n = a.length;
     for(int layer = 0 ; layer < n/2 ; layer++){
       int first = layer;
@@ -36,18 +36,36 @@ public class Q6 {
     }
   }
 
-  public int[][] generateMatrix(int n){
+  public static int[][] generateMatrix(int n){
     int a[][] = new int[n][n];
 
     for(int i = 0 ; i < n; i++){
       for(int j = 0; j < n ; j++){
         Random r = new Random();
-        a[i][j] = r.nextInt();
+        a[i][j] = Math.abs(r.nextInt())%100;
       }
     }
+    return a;
+  }
+
+  public static void printMatrix(int a[][]){
+    System.out.println("Printing matrix->");
+    System.out.println("---------------------------------");
+    for (int i = 0 ; i < a.length ; i++){
+      for( int j = 0; j < a[0].length; j++){
+        System.out.print(a[i][j] + "\t");
+      }
+      System.out.println();
+    }
+    System.out.println("---------------------------------");
   }
 
   public static void main(String args[]){
-
+    int a[][] = generateMatrix(5);
+    printMatrix(a);
+    doRotation(a ,2);
+    printMatrix(a);
+    doRotation(a ,2);
+    printMatrix(a);
   }
 }
